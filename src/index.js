1fd124ea2037630ea36fcc948a4f97b92ea39c63
Pablo Acosta <pablo.acosta@nftstudios.io>
2023-10-15 01:18:33 -0300

    feat: :sparkles: Mods for deploy.

--- src/index.js
+++ src/index.js
@@ -3,10 +3,11 @@ const express = require("express");
 const cors = require("cors");
 const bcfetch = require("bandcamp-fetch");
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 app.use(cors());
+app.use(express.static("dist"));
 
 const getArtistUrl = async (params) => {
   const artistUrl = new Promise((resolve, reject) => {
